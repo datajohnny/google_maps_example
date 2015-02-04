3b06a06d8e28fb29610fa415cb6e1eb084046943
Johnny Mejias <jmejias17@gmail.com>
2015-02-04 01:10:49 -0600

    WIP google-maps

--- config/environment.js
+++ config/environment.js
@@ -19,6 +19,20 @@ module.exports = function(environment) {
     }
   };
 
+  ENV.googleMap = {
+    key: 'AIzaSyCX6QVCjLkjgbu9rMhKVJd4C0dXdwh_L_Q'
+  };
+
+  ENV.contentSecurityPolicy = {
+  'default-src': "'none'",
+  'script-src': "'self' 'unsafe-eval' https://cdn.mxpnl.com http://maps.googleapis.com http://maps.gstatic.com http://mt0.googleapis.com http://mt1.googleapis.com", // Allow scripts from https://cdn.mxpnl.com
+  'font-src': "'self' http://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
+  'connect-src': "'self' https://api.mixpanel.com http://custom-api.local", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
+  'img-src': "'self' http://maps.googleapis.com http://maps.gstatic.com http://mt0.googleapis.com http://mt1.googleapis.com http://khm0.googleapis.com http://khm1.googleapis.com",
+  'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com 
+  'media-src': "'self'"
+}
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
@@ -35,12 +49,10 @@ module.exports = function(environment) {
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
-
     ENV.APP.rootElement = '#ember-testing';
   }
 
   if (environment === 'production') {
-
   }
 
   return ENV;
